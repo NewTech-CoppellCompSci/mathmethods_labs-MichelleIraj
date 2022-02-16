f5f625eaf4e9e51a47d48d2faa6bae2265de9130
Michelle <mraj@newtech.coppellisd.com>
2022-02-15 23:49:28 -0600

    math method labs

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -1,6 +1,6 @@
 package labs;
 
-
+import java.util.Scanner;
 
 public class Lab01 {
 
@@ -17,7 +17,18 @@ public class Lab01 {
 	
 	public static void problem01() {
 		
+		Scanner inKey = new Scanner(System.in); //creates the keyboard
 		
+		System.out.print("Enter a positive integer: "); //informs the user of what data they should input
+		int integer = inKey.nextInt(); //collects the data and stores it into the variable "integer"
+		
+		System.out.print("Enter another positive integer: "); 
+		int integer2 = inKey.nextInt();
+		
+		double product = Math.pow(integer, integer2); //math formula for caluclating the exponents 
+		System.out.print(integer + "^" + integer2 + " = " + product); //printing our the final statement 
+		
+		System.out.println("\n");
 		
 	}
 	
@@ -26,8 +37,15 @@ public class Lab01 {
 	
 	public static void problem02() {
 		
+		Scanner inKey = new Scanner(System.in); //creates the keyboard
+		System.out.print("Enter a positive integer: "); //informs the user of what data they should input
+		int num = inKey.nextInt(); //collects the data and stores it into the variable "num"
+
+		double product = Math.sqrt(num);
+		System.out.print("The square root of " + num + " is " + product);
 		
-		
+		System.out.println("\n");
+
 	}
 
 	
@@ -35,8 +53,24 @@ public class Lab01 {
 
 	public static void problem03() {
 		
+		Scanner inKey = new Scanner(System.in); //creates the keyboard
+		System.out.print("Length of side A: "); //informs the user of what data they should input
+		int a = inKey.nextInt(); //collects the data and stores it into the variable "a"
+
+		System.out.print("Length of side B: "); 
+		int b = inKey.nextInt(); 
+
+		double length1 = Math.pow(a, 2);
+		double length2 = Math.pow(b, 2);
+		
+		double length3 = length1 + length2;
+		
+		double hypo = Math.sqrt(length3);
 		
 		
+		System.out.print("Hypotenuse = " + hypo);
+		
+		System.out.println("\n");
 
 	}
 
@@ -45,13 +79,44 @@ public class Lab01 {
 	
 	public static void problem04() {
 		
+		Scanner inKey = new Scanner(System.in); //creates the keyboard
+		
+		System.out.print("Enter an integer: ");
+		int x1 = inKey.nextInt();
+		
+		int max = x1;
+		int min = x1; 
+		
+		boolean loop = true; 
 		
 		
+		while (loop == true) {
+			
+			System.out.print("Enter an integer: ");
+			int x = inKey.nextInt();
+		
+			if (x == 0) {
+				loop = false; 
+			}
+			
+			if (x != 0) {
+				
+				if (x < min) {
+					min = x; 
+				}
+				if (x > max) {
+					max = x; 
+				}
+				
+			}
+			
+			
+		}
+		
+		System.out.println("Max: " + max);
+		System.out.println("Min: " + min);
+
 	}
-	
-	
-	
-	
 	
 	
 }
--- src/labs/Lab02.java
+++ src/labs/Lab02.java
@@ -1,6 +1,6 @@
 package labs;
 
-
+import java.util.Scanner;
 
 public class Lab02 {
 
@@ -17,18 +17,44 @@ public class Lab02 {
 	
 	public static void problem01() {
 		
+		Scanner inKey = new Scanner(System.in); 
 		
+		System.out.print("Enter a positive integer: ");
+		int num = inKey.nextInt();
 		
+		System.out.print("Enter another positive integer: ");
+		int num2 = inKey.nextInt(); 
+		
+		int max = Math.max(num, num2);
+		int min = Math.min(num, num2); 
+		int scope = max - min + 1; 
+		
+		for (int i = 1; i <= 10; i++) {
+			
+			int randomNum = (int) (Math.random()* scope + min);
+			 System.out.print(randomNum + " ");
+		}
+		
+		System.out.print("\n");
 		
 	}
 	
 	
-	
-	
 	public static void problem02() {
 		
+		Scanner inKey = new Scanner(System.in);
 		
+		System.out.print("Enter the radius value of the cylinder: ");
+		double radius = inKey.nextDouble();
 		
+		System.out.print("Enter the height value of the cylinder: ");
+		double height = inKey.nextDouble();
+		
+		double v = (Math.PI)*(Math.pow(radius, 2))*(height);
+		
+		System.out.print(v);
+		
+		System.out.print("\n");
 	}
 
 	
@@ -36,8 +62,27 @@ public class Lab02 {
 
 	public static void problem03() {
 
+		Scanner inKey = new Scanner(System.in); 
 		
+		System.out.print("Enter x1: ");
+		double x1 = inKey.nextDouble();
 		
+		System.out.print("Enter y1: ");
+		double y1 = inKey.nextDouble();
+		
+		System.out.print("Enter x2: ");
+		double x2 = inKey.nextDouble();
+		
+		System.out.print("Enter y2: ");
+		double y2 = inKey.nextDouble();
+		
+		double x = Math.pow((x1 - x2), 2);
+		
+		double y = Math.pow((y1 - y2), 2);
+		
+		double formula = Math.sqrt(x + y);
+		
+		System.out.print("Distance between the points: " + formula);
 	}
 
 
@@ -45,9 +90,30 @@ public class Lab02 {
 	
 	public static void problem04() {
 		
+		Scanner inKey = new Scanner(System.in); 
 		
+		System.out.print("Enter a-value: ");
+		double a = inKey.nextDouble();
 		
+		System.out.print("Enter b-value: ");
+		double b = inKey.nextDouble();
 		
+		System.out.print("Enter c-value: ");
+		double c = inKey.nextDouble();
+		
+		double s1 = Math.sqrt(Math.pow(b, 2)-(4*(a * c)));
+		
+		double s2 = (b * (-1)) + s1;
+		
+		double s3 = (b * (-1)) - s1;
+		
+		double s4 = s2/(2*a);
+		
+		double s5 = s3/(2*a);
+		
+		System.out.println("x1 = " + s4);
+		System.out.println("x2 = " + s5);
+
 	}
 	
 	
