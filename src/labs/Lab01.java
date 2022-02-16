1c35afc63745cf7639fcbd06a04a80e7052d5b0f
Michelle <mraj@newtech.coppellisd.com>
2022-02-16 13:33:12 -0600

    labs w/ comments

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -41,8 +41,8 @@ public class Lab01 {
 		System.out.print("Enter a positive integer: "); //informs the user of what data they should input
 		int num = inKey.nextInt(); //collects the data and stores it into the variable "num"
 
-		double product = Math.sqrt(num);
-		System.out.print("The square root of " + num + " is " + product);
+		double product = Math.sqrt(num); //calculates the squareroot 
+		System.out.print("The square root of " + num + " is " + product); //prints out the squareroot of the number
 		
 		System.out.println("\n");
 
@@ -60,15 +60,15 @@ public class Lab01 {
 		System.out.print("Length of side B: "); 
 		int b = inKey.nextInt(); 
 
-		double length1 = Math.pow(a, 2);
-		double length2 = Math.pow(b, 2);
+		double length1 = Math.pow(a, 2); //calculating the number squared 
+		double length2 = Math.pow(b, 2); //calculating the number squared 
 		
-		double length3 = length1 + length2;
+		double length3 = length1 + length2; //adding the two lengths for the formula 
 		
-		double hypo = Math.sqrt(length3);
+		double hypo = Math.sqrt(length3); //calculating the hypotenuse 
 		
 		
-		System.out.print("Hypotenuse = " + hypo);
+		System.out.print("Hypotenuse = " + hypo); //printing out the hypotenuse value 
 		
 		System.out.println("\n");
 
@@ -81,16 +81,26 @@ public class Lab01 {
 		
 		Scanner inKey = new Scanner(System.in); //creates the keyboard
 		
-		System.out.print("Enter an integer: ");
-		int x1 = inKey.nextInt();
+		System.out.print("Enter an integer: "); //asks the user to input something 
+		int x1 = inKey.nextInt(); //collecting and storing the data 
 		
-		int max = x1;
-		int min = x1; 
+		int max = x1; //giving the max a value
+		int min = x1; //giving the max a value 
 		
-		boolean loop = true; 
+		boolean loop = true; //used to control the loop 
 		
 		
-		while (loop == true) {
+		/*
+		 * the while loop condition is so it enters the loop 
+		 * asks the user to input data 
+		 * collects and stores the data
+		 * the first if statement is used for if the user enters 0 
+		 * it turns the boolean to false so it leaves the loop(s)
+		 * the second loop is for when the user doesn't enter zero 
+		 * it will calculate and store the min or max 
+		 */
+		
+		while (loop == true) { 
 			
 			System.out.print("Enter an integer: ");
 			int x = inKey.nextInt();
@@ -113,8 +123,8 @@ public class Lab01 {
 			
 		}
 		
-		System.out.println("Max: " + max);
-		System.out.println("Min: " + min);
+		System.out.println("Max: " + max); //printing out the max 
+		System.out.println("Min: " + min); //printing out the min
 
 	}
 	
--- src/labs/Lab02.java
+++ src/labs/Lab02.java
@@ -17,22 +17,22 @@ public class Lab02 {
 	
 	public static void problem01() {
 		
-		Scanner inKey = new Scanner(System.in); 
+		Scanner inKey = new Scanner(System.in); //creates the keyboard 
 		
-		System.out.print("Enter a positive integer: ");
-		int num = inKey.nextInt();
+		System.out.print("Enter a positive integer: "); //asks the user for them to input data
+		int num = inKey.nextInt(); //collects the data and stores it 
 		
 		System.out.print("Enter another positive integer: ");
 		int num2 = inKey.nextInt(); 
 		
-		int max = Math.max(num, num2);
-		int min = Math.min(num, num2); 
-		int scope = max - min + 1; 
+		int max = Math.max(num, num2); //calculates the max between the two integers 
+		int min = Math.min(num, num2); //calculates the min between the two integers 
+		int scope = max - min + 1; //variable created for the range 
 		
-		for (int i = 1; i <= 10; i++) {
+		for (int i = 1; i <= 10; i++) { //conf=dition is made so it only prints the random numbers 10 times 
 			
-			int randomNum = (int) (Math.random()* scope + min);
-			 System.out.print(randomNum + " ");
+			int randomNum = (int) (Math.random()* scope + min); //random number genrator 
+			 System.out.print(randomNum + " "); //prints the random number 
 		}
 		
 		System.out.print("\n");
@@ -42,17 +42,17 @@ public class Lab02 {
 	
 	public static void problem02() {
 		
-		Scanner inKey = new Scanner(System.in);
+		Scanner inKey = new Scanner(System.in); //creates the keyboard 
 		
-		System.out.print("Enter the radius value of the cylinder: ");
-		double radius = inKey.nextDouble();
+		System.out.print("Enter the radius value of the cylinder: "); //asks the user to input data
+		double radius = inKey.nextDouble(); //collects and stores the data 
 		
 		System.out.print("Enter the height value of the cylinder: ");
 		double height = inKey.nextDouble();
 		
-		double v = (Math.PI)*(Math.pow(radius, 2))*(height);
+		double v = (Math.PI)*(Math.pow(radius, 2))*(height); //formula for calculating the cyclinder's volume 
 		
-		System.out.print(v);
+		System.out.print(v); //prints out the volume 
 		
 		System.out.print("\n");
 	}
@@ -62,27 +62,27 @@ public class Lab02 {
 
 	public static void problem03() {
 
-		Scanner inKey = new Scanner(System.in); 
+		Scanner inKey = new Scanner(System.in); //creates the keyboard 
 		
-		System.out.print("Enter x1: ");
-		double x1 = inKey.nextDouble();
+		System.out.print("Enter x1: "); //asks the user for the first x-coordinate 
+		double x1 = inKey.nextDouble(); //collects and stores the data in a variable 
 		
-		System.out.print("Enter y1: ");
-		double y1 = inKey.nextDouble();
+		System.out.print("Enter y1: "); //asks the user for the first y-coordinate
+		double y1 = inKey.nextDouble(); //collects and stores the data in a variable 
 		
-		System.out.print("Enter x2: ");
-		double x2 = inKey.nextDouble();
+		System.out.print("Enter x2: "); //asks the user for the second x-coordinate 
+		double x2 = inKey.nextDouble(); //collects and stores the data in a variable 
 		
-		System.out.print("Enter y2: ");
-		double y2 = inKey.nextDouble();
+		System.out.print("Enter y2: "); //asks the user for the second y-coordinate
+		double y2 = inKey.nextDouble(); //collects and stores the data in a variable 
 		
-		double x = Math.pow((x1 - x2), 2);
+		double x = Math.pow((x1 - x2), 2); //calculates the differnce between the x's and squares it 
 		
-		double y = Math.pow((y1 - y2), 2);
+		double y = Math.pow((y1 - y2), 2); //calculates the differnce between the y's and squares it 
 		
-		double formula = Math.sqrt(x + y);
+		double formula = Math.sqrt(x + y); //finally adds the y and x value and takes the squareroot of it
 		
-		System.out.print("Distance between the points: " + formula);
+		System.out.print("Distance between the points: " + formula); //printing out the final answer 
 	}
 
 
@@ -90,28 +90,28 @@ public class Lab02 {
 	
 	public static void problem04() {
 		
-		Scanner inKey = new Scanner(System.in); 
+		Scanner inKey = new Scanner(System.in); //creates the keyboard 
 		
-		System.out.print("Enter a-value: ");
-		double a = inKey.nextDouble();
+		System.out.print("Enter a-value: "); //asks the user to input data 
+		double a = inKey.nextDouble(); //collects and stores the data in variable "a"
 		
-		System.out.print("Enter b-value: ");
-		double b = inKey.nextDouble();
+		System.out.print("Enter b-value: "); //asks the user to input data 
+		double b = inKey.nextDouble(); //collects and stores the data in variable "b"
 		
-		System.out.print("Enter c-value: ");
-		double c = inKey.nextDouble();
+		System.out.print("Enter c-value: "); //asks the user to input data 
+		double c = inKey.nextDouble(); //collects and stores the data in variable "c"
 		
-		double s1 = Math.sqrt(Math.pow(b, 2)-(4*(a * c)));
+		double s1 = Math.sqrt(Math.pow(b, 2)-(4*(a * c))); //formula for the first section of the formula under the square root
 		
-		double s2 = (b * (-1)) + s1;
+		double s2 = (b * (-1)) + s1; //makes "b" negative and adds it to the first section
 		
-		double s3 = (b * (-1)) - s1;
+		double s3 = (b * (-1)) - s1; //makes "b" negative and subtracts it from the first section
 		
-		double s4 = s2/(2*a);
+		double s4 = s2/(2 * a); //dividing the second section by 2 multiplied by "a"
 		
-		double s5 = s3/(2*a);
+		double s5 = s3/(2 * a); //dividing the third section by 2 multiplied by "a"
 		
-		System.out.println("x1 = " + s4);
+		System.out.println("x1 = " + s4); //printing out the final answer
 		System.out.println("x2 = " + s5);
 
 	}
